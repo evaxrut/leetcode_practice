df1ded926240a24a032ebefe9837219782ff0085
evaxrut <eva.rut.omarsdottir@gmail.com>
2026-05-27 18:27:44 +0200

    find max avg subarr

--- maximum_average_subarray.cpp
+++ maximum_average_subarray.cpp
@@ -5,5 +5,24 @@ class Solution {
     double findMaxAverage(
         vector<int>& nums,
         int k
-    ) {}
+    ) {
+        double sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        double max_sum = sum;
+
+        for (int i = k; i < nums.size(); i++) {
+            sum -= nums[i - k];
+            sum += nums[i];
+
+            if (max_sum < sum) {
+                max_sum = sum;
+            }
+        }
+
+        return max_sum / k;
+    }
 };
